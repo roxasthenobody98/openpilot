9fe0343379b4018d57169a585a7aa228d06b849c
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-02-28 16:33:58 -0600

    Update carcontroller.py

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -1,7 +1,7 @@
 from cereal import car
 from selfdrive.config import Conversions as CV
-from selfdrive.car.ford.fordcan import make_can_msg, create_steer_command, create_lkas_ui, \
-                                       spam_cancel_button
+from selfdrive.car import make_can_msg
+from selfdrive.car.ford.fordcan import create_steer_command, create_lkas_ui, spam_cancel_button
 from opendbc.can.packer import CANPacker
 
 
