5dcf17dfdc960ee897ee8d1d91430cde527b2b2f
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-02-29 13:05:23 -0600

    Update safety_ford.h

--- panda/board/safety/safety_ford.h
+++ panda/board/safety/safety_ford.h
@@ -112,6 +112,6 @@ const safety_hooks ford_hooks = {
   .rx = ford_rx_hook,
   .tx = ford_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
-  .ignition = default_ign_hook,
+  //.ignition = default_ign_hook,
   .fwd = ford_fwd_hook,
 };
