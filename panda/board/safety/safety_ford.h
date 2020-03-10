f6ede07bc1972d4c9e3740b6db80a04ce18f5911
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-03-10 13:45:49 -0500

    Revert "Ford: Add fwd hook"

--- panda/board/safety/safety_ford.h
+++ panda/board/safety/safety_ford.h
@@ -88,7 +88,7 @@ static int ford_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
       if ((GET_BYTE(to_send, 0) & 0xF0) != 0xF0) {
         tx = 1; //always allow 0
       }
-   tx = 1; 
+	  tx = 1; 
     }
   }
 
@@ -98,37 +98,18 @@ static int ford_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
     if ((GET_BYTE(to_send, 3) & 0x30) != 0) {
       tx = 1; //always allow 0
     }
-  tx = 1;
+	tx = 1;
   }
   // 1 allows the message through
   return tx;
 }
 
 // TODO: keep camera on bus 2 and make a fwd_hook
-static int ford_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
-  int bus_fwd = -1;
-  int addr = GET_ADDR(to_fwd);
-	
-  if (!relay_malfunction) {
-  // forward CAN 0 -> 2 so stock LKAS camera sees messages
-    if (bus_num == 0) {
-      bus_fwd = 2;
-    }
-    // forward all messages from camera except Lane_Keep_Assist_Control and Lane_Keep_Assist_Ui
-    if ((bus_num == 2) && (addr != 0x3CA || 0x3D8)) {
-      bus_fwd = 0;
-    }
-    //int block_msg = (addr == 0x3CA) || (addr == 0x3D8);
-    //if (!block_msg) {
-    //  bus_fwd = 0;
-    //}  
-  }
-  return bus_fwd;
-}
+
 const safety_hooks ford_hooks = {
   .init = nooutput_init,
   .rx = ford_rx_hook,
   .tx = ford_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
-  .fwd = ford_fwd_hook,
+  .fwd = default_fwd_hook,
 };
