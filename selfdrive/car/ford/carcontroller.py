5822a4b822078b682af99fd4cc30949e5acc482f
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-08-14 13:07:55 -0500

    Update carcontroller.py

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -56,7 +56,7 @@ class CarController():
         if CS.out.genericToggle == 1:
           self.lkasToggle += 1
         if self.lkasToggle > 6:
-          self.lkasToggle == 1
+          self.lkasToggle = 1
         self.lkas_action = self.lkasToggle #6 Finished 5 NotAccessible 4 ApaCancelled 2 On 1 Off  
         #if self.lkasCounter < COUNTER_MAX:
         #  can_sends.append(create_steer_command(self.packer, apply_steer, enabled, CS.lkas_state, CS.out.steeringAngle, curvature, self.lkas_action))
