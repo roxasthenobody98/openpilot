15f1bcecd26ec454e7fff98c4b4e8da045fd8f93
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-08-23 18:57:15 -0500

    change strategy

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -55,18 +55,20 @@ class CarController():
             self.sappConfig = 86
             self.angleReq = 1
           if CS.sappHandshake == 2:
-            self.sappConfig = 224
+            self.sappConfig = 86
             self.angleReq = 1
           self.sappConfig_last = self.sappConfig
           self.angleReq_last = self.angleReq
-        if CS.out.vEgo > 1 and CS.sappHandshake == 2:
-          self.sappConfig = 224
+        if CS.out.vEgo > 1 and CS.sappHandshake == 2 and not enabled:
+          self.sappConfig = 86
           self.angleReq = 1
       if (frame % 2) == 0:
       #Stock IPMA Message is 33Hz. PSCM accepts commands at max 44Hz. 
         curvature = self.vehicle_model.calc_curvature(actuators.steerAngle*np.pi/180., CS.out.vEgo)
         self.lkas_action = 0 #6 Finished 5 NotAccessible 4 ApaCancelled 2 On 1 Off  
         if enabled:  
+          self.sappConfig = 224
+          self.angleReq = 1
           if self.lastAngle * apply_steer > 0.:
             angle_rate_lim = interp(CS.out.vEgo, ANGLE_DELTA_BP, ANGLE_DELTA_V)
           else:
