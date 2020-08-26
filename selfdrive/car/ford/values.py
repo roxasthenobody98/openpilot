80fbe7bc016e77bfecd665b4bb2a4443047b8419
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-08-26 01:29:27 -0500

    add fpv2 support

--- selfdrive/car/ford/values.py
+++ selfdrive/car/ford/values.py
@@ -103,7 +103,31 @@ FINGERPRINTS = {
 ECU_FINGERPRINT = {
   Ecu.fwdCamera: [970, 973, 984, 985]
 }
-
+FW_VERSIONS = {
+  CAR.F150: {
+    (Ecu.ipma, 0x706, None): [
+      b'FL3T-14G019-DE\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+    ],
+    (Ecu.pscm, 0x730, None): [
+      b'GL34-14D003-AD\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    ],
+    (Ecu.abs, 0x760, None): [
+      b'FL34-2D053-BA\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    ],
+    (Ecu.ccm, 0x764, None): [
+      b'FL3T-14D049-AF\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    ],
+    (Ecu.pcm, 0x7e0, None): [
+      b'FL3A-14C204-ABL\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    ],
+    (Ecu.rcm, 0x737, None): [
+      b'GR3T-14C028-AA\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    ],
+    (Ecu.ipc, 0x720, None): [
+      b'FL3T-14C026-CJ\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    ],
+  }
+}
 #FEATURES = {
   #"use_op_longitudinal": [CAR.F150SG, CAR.FUSIONSG],
 #}
