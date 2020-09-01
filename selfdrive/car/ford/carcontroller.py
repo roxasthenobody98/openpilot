d36157e43cc8396ef5dd6a1e556f296f9abeb6e0
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-08-31 20:04:09 -0500

    So many bad indents

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -56,13 +56,13 @@ class CarController():
           self.apaCounter = 0
           self.apaCounter += 1
           self.sappConfig = 168
-	  if CS.sappHandshake == 1 && self.apaCounter == 6:
-	    self.sappConfig = 200
-	    if self.apaCounter == 13:
-	      self.angleReq = 1
-	    if CS.sappHandshake == 2 && self.apaCounter == 14:
-	      self.sappConfig = 226
-	      self.apaCounter = 0
+          if CS.sappHandshake == 1 && self.apaCounter == 6:
+            self.sappConfig = 200
+          if self.apaCounter == 13:
+            self.angleReq = 1
+          if CS.sappHandshake == 2 && self.apaCounter == 14:
+            self.sappConfig = 2
+            self.apaCounter = 0
         #if CS.out.vEgo < 1:
         #  self.sappConfig = 70
         #  if CS.sappHandshake in [0,1]:
