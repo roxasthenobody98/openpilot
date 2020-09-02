bb12354811d9dd2ebbbb7692d2a62ffe4fe10797
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-09-02 00:48:19 -0500

    Update carcontroller.py

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -77,7 +77,7 @@ class CarController():
           if CS.sappHandshake == 2 and self.sappConfig_last == 20:
             self.sappConfig = 20
             self.angleReq = 1
-        print("Handshake:", CS.sappHandshake, "Config:", self.sappConfig_last, "Counter:", self.apaCounter, "PAM Config:", CS.sappConfig)
+        print("Handshake:", CS.sappHandshake, "Config:", self.sappConfig_last, "Counter:", self.apaCounter, "AngleRequest:", self.angleReq)
       #Stock IPMA Message is 33Hz. PSCM accepts commands at max 44Hz. 
         curvature = self.vehicle_model.calc_curvature(actuators.steerAngle*np.pi/180., CS.out.vEgo)
         self.lkas_action = 0 #6 Finished 5 NotAccessible 4 ApaCancelled 2 On 1 Off  
