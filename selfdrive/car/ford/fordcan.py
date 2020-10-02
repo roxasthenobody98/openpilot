92d03d82b42136eb23f6cb936ab961f426ba502f
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2020-10-02 12:13:21 -0500

    did an oopsie with the latest commit.

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -1,13 +1,12 @@
 from common.numpy_fast import clip
 from selfdrive.car.ford.values import MAX_ANGLE
 
-def create_steer_command(packer, angle_cmd, enabled, angle_steers, lkas_action, angleReq, sappConfig, sappChime):
+def create_steer_command(packer, angle_cmd, enabled, lkas_state, angle_steers, curvature, lkas_action):
   """Creates a CAN message for the Ford Steer Command."""
 
   #if enabled and lkas available:
-  if enabled: # and (frame % 500) >= 3:
+  if enabled and lkas_state in [2,3]: # and (frame % 500) >= 3:
     action = lkas_action
-    angle_cmd = angle_steers/MAX_ANGLE
   else:
     action = 0xf
     angle_cmd = angle_steers/MAX_ANGLE
@@ -15,13 +14,12 @@ def create_steer_command(packer, angle_cmd, enabled, angle_steers, lkas_action, 
   angle_cmd = clip(angle_cmd * MAX_ANGLE, - MAX_ANGLE, MAX_ANGLE)
 
   values = {
-    "ApaSys_D_Stat": action,
-    "EPASExtAngleStatReq": angleReq,
-    "ExtSteeringAngleReq2": angle_cmd,
-    "SAPPStatusCoding": sappConfig,
-    "ApaChime_D_Rq": sappChime,
+    "Lkas_Action": action,
+    "Lkas_Alert": 0xe,             # no alerts
+    "Lane_Curvature": clip(curvature, -0.03, 0.03),   # is it just for debug?
+    "Steer_Angle_Req": angle_cmd
   }
-  return packer.make_can_msg("ParkAid_Data", 0, values)
+  return packer.make_can_msg("Lane_Keep_Assist_Control", 0, values)
 
 #def create_lkas_status(packer, enabled, lkasState, steer_pressed, steer_error):
 #  """Creates a CAN message for the Ford LKAS State"""
