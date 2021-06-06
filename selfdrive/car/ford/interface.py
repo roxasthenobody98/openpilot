67ea579c7ca9ed097bbefa701a89c6b085827c3f
roxasthenobody98 <61211864+roxasthenobody98@users.noreply.github.com>
2021-06-06 01:24:26 -0500

    hud things

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -136,7 +136,10 @@ class CarInterface(CarInterfaceBase):
   def apply(self, c):
 
     can_sends = self.CC.update(c.enabled, self.CS, self.frame, c.actuators,
-                               c.hudControl.visualAlert, c.cruiseControl.cancel)
+                               c.hudControl.visualAlert, c.cruiseControl.cancel,
+                               c.hudControl.leftLaneVisible, c.hudControl.rightLaneVisible,
+                               c.hudControl.leadVisible, c.hudControl.leftLaneDepart,
+                               c.hudControl.rightLaneDepart)
 
     self.frame += 1
     return can_sends
